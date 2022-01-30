3e2bfede2d4ca9152de48fd015bfe04ea3489bba
vtrivellato <victor.trivellato@hotmail.com>
2022-01-30 11:10:27 -0300

    initial commit

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,33 @@
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: Array<GenreResponseProps>
+  selectedGenreId: number
+  handleClickButton: Function
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar({ genres, selectedGenreId, handleClickButton }: SideBarProps) {  
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
+}
